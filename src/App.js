fdfa60b601e9ea5b370ca68d1c363605d65e6dfc
Mj <mjichsan@gmail.com>
2022-07-06 15:32:34 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import "./css/App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Dashboard from "./pages/dashboard";
-import Mobile from "./pages/mobile";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Dashboard} />
-        <Route exact path="/mobile" component={Mobile} />
       </Switch>
     </Router>
   );
